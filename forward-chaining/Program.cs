d5db59a1ee0e0397053e78392dcab0269b6b1801
Paulius <pauliusval99@hotmail.com>
2018-05-12 13:20:36 +0300

    pretty much done

--- forward-chaining/Program.cs
+++ forward-chaining/Program.cs
@@ -10,6 +10,10 @@ namespace forward_chaining
     {
         static StreamWriter writer;
         static List<Rule> rules = new List<Rule>();
+        static bool[] flag1;
+        static bool[] flag2;
+        static List<int> rulesUsed = new List<int>();
+        static List<char> initialFacts;
         static List<char> facts = new List<char>();
         static char goal;
         static void Main(string[] args)
@@ -36,10 +40,113 @@ namespace forward_chaining
                 Console.ReadKey();
                 return;
             }
+            Write("PART 1. Data\n\n");
+            Write("  1) Rules\n");
+            for (int i = 0; i < rules.Count; i++)
+            {
+                Write("     R{0}: {1} -> {2}\n", i+1, String.Join(", ", rules[i].Left), rules[i].Right);
+            }
+            Write("\n  2) Facts\n     {0}\n", String.Join(", ", facts));
+            Write("\n  3) Goal\n     {0}\n\n", goal);
+
+            Write("PART 2. Trace\n\n");
+
+            initialFacts = new List<char>(facts);
+            facts.Clear();
+
+            flag1 = new bool[rules.Count];
+            flag2 = new bool[rules.Count];
+            for(int i=0; i<rules.Count; i++)
+            {
+                flag1[i] = flag2[i] = false;
+            }
+
+            bool success = Solve();
+
+            Write("PART 3. Results\n");
+            if(initialFacts.Contains(goal))
+            {
+                Write("  Goal {0} in facts. Empty path.");
+            }
+            else if(success)
+            {
+                Write("  1) Goal {0} achieved.\n  2) Path: {1}.", goal, String.Join(", ", from r in rulesUsed select String.Format("R{0}", r+1)));
+                // TODO: print out path
+            }
+            else
+            {
+                Write("  No path found.");
+            }
+
             writer.Flush();
             #if DEBUG
                 Console.ReadKey();
             #endif
+        }
+
+        static int iteration = 1;
+        static bool Solve()
+        {
+            if (initialFacts.Contains(goal))
+            {
+                Write("  Goal achieved.");
+                return true;
+            }
+            while(!facts.Contains(goal))
+            {
+                Write("ITERATION {0}\n", iteration++);
+                bool success = false;
+                for(int i=0; i<rules.Count; i++)
+                {
+                    Write("    R{0}:{1}->{2} ", i + 1, String.Join(',', rules[i].Left), rules[i].Right);
+                    if(flag1[i])
+                    {
+                        Write("skip, because flag1 raised.\n");
+                        continue;
+                    }
+                    if(flag2[i])
+                    {
+                        Write("skip, because flag2 raised.\n");
+                        continue;
+                    }
+
+                    List<char> missing = new List<char>();
+                    foreach(char node in rules[i].Left)
+                    {
+                        if (!facts.Contains(node) && !initialFacts.Contains(node))
+                            missing.Add(node);
+
+                    }
+                    // can be applied
+                    if(missing.Count == 0)
+                    {
+                        if(facts.Contains(rules[i].Right) || initialFacts.Contains(rules[i].Right))
+                        {
+                            Write("not applied, because RHS is in facts. Raise flag2.\n");
+                            flag2[i] = true;
+                            continue;
+                        }
+                        else
+                        {
+                            facts.Add(rules[i].Right);
+                            Write("apply. Raise flag1. Facts {0} and {1}.\n", String.Join(", ", initialFacts), String.Join(", ", facts));
+                            flag1[i] = true;
+                            rulesUsed.Add(i);
+                            success = true;
+                            if(rules[i].Right == goal)
+                                Write("    Goal achieved.\n");
+                            break;
+                        }
+                    }
+
+                    //wasn't applied...
+                    Write("not applied, because of lacking {0}.\n", String.Join(", ", missing));
+
+                }
+                Write("\n");
+                if (!success) return false;
+            }
+            return true;
         }
 
         public static void Write(string format, params object[] arg)
@@ -109,5 +216,6 @@ namespace forward_chaining
             }
         }
         
+
     }
 }
